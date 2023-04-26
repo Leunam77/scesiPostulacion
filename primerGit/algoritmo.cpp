5c03870554622969664e11f229abf9e837a6cd0a
Leunam77 <juan.ma6531@gmail.com>
2023-04-26 17:05:37 -0400

    mayorMenor

--- primerGit/algoritmo.cpp
+++ primerGit/algoritmo.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 int main(){
     string nombre;
+    int a, b;
+    cout << "Ingrese su nombre: " << "\n";
     cin >> nombre;
     cout << "Mi nombre es: " << nombre << "\n";
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+    if(a>b){
+        cout << "El mayor es: " << a << "\n";
+    }else{
+        cout << "El mayor es: " << b << "\n";
+    }
     return 0;
 }
